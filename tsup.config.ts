79a65d8892d34294f6e21d489d60128667d1b5e2
kkMihai <kkmihai@proton.me>
2026-03-14 01:21:33 +0200

    chore(build): disable sourcemap generation in tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
   splitting: true,
   treeshake: true,
